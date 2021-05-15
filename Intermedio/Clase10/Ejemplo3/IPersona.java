8ba2e16462c75a0152d6a0eeef3a64e450f0b4f8
ijchavez <72580524+ijchavez@users.noreply.github.com>
2021-05-15 12:28:54 -0300

    Delete Intermedio directory

--- Intermedio/Clase10/Ejemplo3/IPersona.java
+++
@@ -1,8 +0,0 @@
-package Intermedio.Clase10.Ejemplo3;
-
-public interface IPersona {
-    //creo la interface
-    void mostrarNombre(String nombre);
-    void mostrarDNI(int dni);
-
-}
--- Intermedio/Clase10/Ejemplo3/IProfesor.java
+++
@@ -1,9 +0,0 @@
-package Intermedio.Clase10.Ejemplo3;
-
-public interface IProfesor extends IPersona {
-
-    void obtenerSalario(float salario);
-    void obtenerMateria(String materia);
-
-
-}
