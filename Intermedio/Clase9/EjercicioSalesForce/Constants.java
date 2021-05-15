eb8e9589f81efd79df150cb461ea4be962e378e6
ijchavez <72580524+ijchavez@users.noreply.github.com>
2021-05-15 12:28:31 -0300

    Delete Intermedio directory

--- Intermedio/Clase9/EjercicioSalesForce/Constants.java
+++
@@ -1,8 +0,0 @@
-package Intermedio.Clase9.EjercicioSalesForce;
-
-public class Constants {
-    public static final String SALESFORCE_URL = "http://www.salesforce.com/";
-    public static final String REGISTRO_TAB = "registro";
-    public static final String MAIN_TAB = "main";
-
-}
