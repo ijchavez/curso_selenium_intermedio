a51e51412886f4fef9b266d926181565cc0c5d6b
ijchavez <72580524+ijchavez@users.noreply.github.com>
2021-05-15 12:32:01 -0300

    Delete Intermedio directory

--- Intermedio/Clase10/Ejemplo2/IVehiculo.java
+++
@@ -1,9 +0,0 @@
-package Intermedio.Clase10.Ejemplo2;
-
-public interface IVehiculo {
-    void encender();
-    void girar();
-    void apagar();
-    void acelerar();
-
-}
--- Intermedio/Clase10/Ejemplo3/IAlumno.java
+++
@@ -1,8 +0,0 @@
-package Intermedio.Clase10.Ejemplo3;
-
-public interface IAlumno extends IPersona{
-
-    void obtenerCurso(String curso);
-    void calcularPromedio(float promedio);
-
-}
